00e817c252d0161803b27f3565c4a14f1517f952
Jordan Poles <jpoles1@gmail.com>
2020-01-13 13:14:23 -0500

    Cleaning up exe and installation script!

--- install.bat
+++ install.bat
@@ -1,1 +1,1 @@
-elevate.exe -k xcopy "dist" "C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Trusty Timer"
+elevate.exe -k xcopy /I /Y "dist" "C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Trusty Timer"
